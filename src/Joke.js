ce10cfcc202246d4be5242a834743987e0643dd4
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-04-03 16:34:28 -0700

    Update Joke.js

--- src/Joke.js
+++ src/Joke.js
@@ -2,22 +2,10 @@ import React, { Component } from "react";
 import "./Joke.css";
 class Joke extends Component {
   render() {
-    const { id, vote, votes, text } = this.props;
+    const { text } = this.props;
 
     return (
       <div className="Joke">
-        <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
-            <i className="fas fa-thumbs-up" />
-          </button>
-
-          <button onClick={evt => vote(id, -1)}>
-            <i className="fas fa-thumbs-down" />
-          </button>
-
-          {votes}
-        </div>
-
         <div className="Joke-text">{text}</div>
       </div>
     );
